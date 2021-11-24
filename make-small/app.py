ae786c16479d4f240910c7854babcab4b7ae2fb3
praful-parmar <prafulparmar.pp62@gmail.com>
2021-11-24 13:31:07 +0530

    app code change

--- make-small/app.py
+++ make-small/app.py
@@ -6,6 +6,49 @@ from functools import wraps
 
 import os
 
+
+
+import boto3
+import base64
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+
+    secret_name = "arn:aws:secretsmanager:us-east-1:501533612229:secret:MyRDSSecret-*"
+    region_name = "us-east-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            raise e
+    else:
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+            print(secret)
+        else:
+            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+
+
+
+
 app = Flask(__name__)
 app.debug = True
 
@@ -303,5 +346,6 @@ def delete_article(id):
 
 
 if __name__ =='__main__':
+    get_secret()
     app.secret_key='secret123'
     app.run(debug=False,host='0.0.0.0',port=5000)
