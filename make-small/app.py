f656ffc34ead1e750323b277ff8ebfe5b07098b8
praful-parmar <prafulparmar.pp62@gmail.com>
2021-11-25 12:18:35 +0530

    python code change

--- make-small/app.py
+++ make-small/app.py
@@ -5,52 +5,35 @@ from passlib.hash import sha256_crypt
 from functools import wraps
 
 import os
-
-
-
 import boto3
-import base64
-from botocore.exceptions import ClientError
-
-
-def get_secret():
-
-    secret_name = os.environ.get('ARN')
-    region_name = "us-east-1"
-
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            raise e
-    else:
-        if 'SecretString' in get_secret_value_response:
-            secret = get_secret_value_response['SecretString']
-            print(secret)
-        else:
-            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
-
-
 
 
 app = Flask(__name__)
 app.debug = True
+
+
+
+
+  
+def get(name, version=None):
+    """Gets the value of a secret.
+
+    Version (if defined) is used to retrieve a particular version of
+    the secret.
+
+    """
+    secrets_client = boto3.client("secretsmanager")
+    kwargs = {'SecretId': name}
+    if version is not None:
+        kwargs['VersionStage'] = version
+    response = secrets_client.get_secret_value(**kwargs)
+    secret=eval(response["SecretString"])
+
+    os.environ['MYSQL_PORT'] = secret["port"]
+    os.environ['MYSQL_PASSWORD'] = secret["password"]
+    os.environ['MYSQL_DB'] = secret["dbInstanceIdentifier"]
+    os.environ['MYSQL_HOST'] = secret["host"]
+    os.environ['MYSQL_USER'] = secret["username"]
 
 
 
@@ -346,6 +329,6 @@ def delete_article(id):
 
 
 if __name__ =='__main__':
-    get_secret()
     app.secret_key='secret123'
+    get("MyRDSSecret")
     app.run(debug=False,host='0.0.0.0',port=5000)
