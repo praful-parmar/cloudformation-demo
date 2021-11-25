dca8e311222d519b3cb47fd6025f0b9aca1b4744
praful-parmar <prafulparmar.pp62@gmail.com>
2021-11-25 14:49:59 +0530

    docker compose change

--- make-small/app.py
+++ make-small/app.py
@@ -16,12 +16,6 @@ app.debug = True
 
   
 def get(name, version=None):
-    """Gets the value of a secret.
-
-    Version (if defined) is used to retrieve a particular version of
-    the secret.
-
-    """
     secrets_client = boto3.client("secretsmanager")
     kwargs = {'SecretId': name}
     if version is not None:
